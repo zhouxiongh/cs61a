baf7891c1857da069bd9a24fb2ff33f6a51dadb2
jason <zhouxiongh@gmail.com>
2020-04-18 16:41:08 +0800

    first commit

---
+++ Lab/lab03/lab03.py
@@ -0,0 +1,57 @@
+def ab_plus_c(a, b, c):
+    """Computes a * b + c.
+
+    >>> ab_plus_c(2, 4, 3)  # 2 * 4 + 3
+    11
+    >>> ab_plus_c(0, 3, 2)  # 0 * 3 + 2
+    2
+    >>> ab_plus_c(3, 0, 2)  # 3 * 0 + 2
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    if b == 0:
+        return c
+    return a + ab_plus_c(a, b - 1, c)
+
+def gcd(a, b):
+    """Returns the greatest common divisor of a and b.
+    Should be implemented using recursion.
+
+    >>> gcd(34, 19)
+    1
+    >>> gcd(39, 91)
+    13
+    >>> gcd(20, 30)
+    10
+    >>> gcd(40, 40)
+    40
+    """
+    "*** YOUR CODE HERE ***"
+    if a % b == 0:
+        return b
+    return gcd(b, a % b)
+
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the
+    number of elements in the sequence.
+
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    "*** YOUR CODE HERE ***"
+    print(n)
+    if n == 1:
+        return 1
+    if n % 2 == 0:
+        return hailstone(n//2) + 1
+    else:
+        return hailstone(n * 3 + 1) + 1
